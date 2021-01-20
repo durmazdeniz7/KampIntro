f55da7f779d634d1d7f86e939f3ad20220a0203f
deniz <deniz@DESKTOP-9KDECH2>
2021-01-20 23:50:38 +0300

    Generic Class

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -17,6 +17,11 @@ namespace GenericsIntro
         {
             T[] tempArray = items;
             items = new T[items.Length+1] ;
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
         }
     }
 }
