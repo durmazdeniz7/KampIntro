ed6e99f05cd9d89febcd4c30e0a8537d79ccc806
deniz <deniz@DESKTOP-9KDECH2>
2021-01-20 23:36:12 +0300

    GenericsClass

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collection
 {
@@ -6,7 +7,36 @@ namespace Collection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string[] isimler = new string[] { 
+            //"Deniz","Durmaz","Sibel","Ayuldar"};
+
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //foreach (var item in isimler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            List<string> isimler2 = new List<string> { 
+            
+            "Deniz","Durmaz","Ramazan","Kürşat"
+            };
+
+
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Baby");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+        
         }
     }
 }
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Deniz Durmaz");
+
+            Console.WriteLine("Hello World!");
+        
+        
+        
+        }
+    }
+}
